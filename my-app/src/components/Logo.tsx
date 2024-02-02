1e7385a82d86ae08d9bbe3cd0f7ab3bfe7e43819
José Alvarez C <alvarez.jo.developer@gmail.com>
2024-02-02 14:35:06 -0600

    Add logo to mobile layout in landing too

--- my-app/src/components/Logo.tsx
+++ my-app/src/components/Logo.tsx
@@ -3,16 +3,21 @@
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
-const Logo = () => {
+interface LogoProps {
+  width?: number;
+  height?: number;
+}
+
+const Logo: React.FC<LogoProps> = ({ width = 300, height = 300 }) => {
   const router = useRouter();
 
   return (
     <Image
       onClick={() => router.push("/")}
       alt="Logo"
-      className="hidden md:block cursor-pointer"
-      height="300"
-      width="300"
+      className="block cursor-pointer"
+      height={width}
+      width={height}
       src="/reading-club-ai-white-logo-new.png"
     ></Image>
   );
