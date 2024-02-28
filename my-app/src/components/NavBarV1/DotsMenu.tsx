d41cd79d1cbac4ac6f0bcfbbd9cd43609be5440c
José Alvarez C <alvarez.jo.developer@gmail.com>
2024-02-28 12:50:26 -0600

    Backup dot menu

---
+++ my-app/src/components/NavBarV1/DotsMenu.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { MoreHorizontal } from "lucide-react";
+import SignOutForm from "./SignOutForm";
+import Link from "next/link";
+
+interface DotsMenuProps {
+  showSignout?: boolean;
+}
+
+const DotsMenu: React.FC<DotsMenuProps> = ({ showSignout = true }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        asChild
+        className="hover:bg-transparent hover:text-white focus-visible:border-0 block lg:hidden"
+      >
+        <Button size="sm" variant={"ghost"}>
+          {/* used to be h-4 w-4 */}
+          <MoreHorizontal className="h-6 w-6" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        // className="w-60"
+        align="end"
+        alignOffset={8}
+        forceMount
+        className="border-0"
+      >
+        {/* <DropdownMenuItem>Item 1</DropdownMenuItem> */}
+        <DropdownMenuItem>
+          <Link href="/settings" className="text-black">
+            Settings
+          </Link>
+        </DropdownMenuItem>
+        {showSignout == true && (
+          <>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>{<SignOutForm />}</DropdownMenuItem>
+          </>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default DotsMenu;
