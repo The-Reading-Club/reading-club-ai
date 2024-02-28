2917dd76729b9ed2558ef1b35832e502dcfc766b
José Alvarez C <alvarez.jo.developer@gmail.com>
2024-02-28 09:20:48 -0600

    Test button to create story on newsfeed

--- my-app/src/app/(server)/(client)/(main)/_components/DraftItem/DraftItem.tsx
+++ my-app/src/app/(server)/(client)/(main)/_components/DraftItem/DraftItem.tsx
@@ -13,6 +13,8 @@ interface DraftItemProps {
   hoverColorClassName?: string;
   titleBgColorClassname?: string;
   showCoverImage?: boolean;
+  authorColorClassname?: string;
+  titleColorClassname?: string;
 }
 
 // const DEFAULT_COVER_URL = "https://www.readingclub.ai/_next/image?url=https%3A%2F%2F0opmmv83e2pndbdg.public.blob.vercel-storage.com%2Fbeta%2Fimages%2F2024-02-06%2F52748-HV7IOqdDaK-Y7ivpxbBS8B4zZFovicu3BtfiBSZhc.png&w=2048&q=75";
@@ -30,6 +32,8 @@ const DraftItem: React.FC<DraftItemProps> = ({
   hoverColorClassName = "bg-accent",
   titleBgColorClassname = "bg-white",
   showCoverImage = true,
+  authorColorClassname = "",
+  titleColorClassname = "",
 }) => {
   return (
     <div
@@ -84,8 +88,8 @@ const DraftItem: React.FC<DraftItemProps> = ({
       >
         {/* <div>{d.coverImage}</div> */}
 
-        <div className="font-bold">{title}</div>
-        <div>{author}</div>
+        <div className={`font-bold ${titleColorClassname}`}>{title}</div>
+        <div className={`${authorColorClassname}`}>{author}</div>
       </div>
     </div>
   );
