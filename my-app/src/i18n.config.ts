b30bb8adf50731a85cf805a1de095fa912ee7baa
José Alvarez C <alvarez.jo.developer@gmail.com>
2024-03-14 10:21:52 -0600

    Deploy japanese

--- my-app/src/i18n.config.ts
+++ my-app/src/i18n.config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
   defaultLocale: "en",
-  locales: ["en", "es", "uk", "zh-CN", "hi", "bn", "pt"],
+  locales: ["en", "es", "uk", "zh-CN", "hi", "bn", "pt", "ja"],
 } as const;
 
 export type Locale = (typeof i18n.locales)[number];
